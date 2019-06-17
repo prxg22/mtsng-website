eb30b3527d0e9d83f6dbc1542268362e5c87dfd1
prxg22 <prxg22@gmail.com>
2019-06-17 01:42:13 -0300

    fix: close header on logo click

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,19 @@ window.onload = () => {
   const headerController = window.HeaderController('header')
   const nav = document.querySelector('header nav')
   const navAnchors = document.querySelectorAll('header nav a')
-  const anchors = Array.prototype.slice.call(navAnchors)
   const menuButton = document.querySelector('#menu_btn')
+  const logo = document.querySelector('a#logo')
 
   menuButton.onclick = () => {
     headerController.toggle()
   }
 
-  nav.onclick = (e) => {
-    if (anchors.includes(e.target)) headerController.close()
+  nav.onclick = ({ target }) => {
+    const anchors = Array.prototype.slice.call(navAnchors)
+    if (anchors.includes(target)) headerController.close()
+  }
+
+  logo.onclick = ({ currentTarget }) => {
+    if (currentTarget === logo) headerController.close()
   }
 }
