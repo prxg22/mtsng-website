4b28833f7677124e57a32636a427e50afbde1fcd
prxg22 <prxg22@gmail.com>
2019-09-16 12:01:34 -0300

    feat: gallery image preview

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -22,9 +22,17 @@ const Gallery = (gallery) => {
   let button
   const showing = { prev: false, next: false }
   const container = gallery.querySelector('.gallery__container')
+  const images = gallery.querySelectorAll('img')
 
   const width = () => gallery.clientWidth
   const isMobile = () => window.innerWidth < 1024
+
+  const previewContainer = document.createElement('div')
+  previewContainer.classList.add('preview-container')
+
+  const labelContainer = document.createElement('p')
+  previewContainer.appendChild(labelContainer)
+  labelContainer.classList.add('label')
 
   const showButton = (dir = 'next') => {
     if (showing[dir] || isMobile()) return
@@ -49,6 +57,35 @@ const Gallery = (gallery) => {
     })
   }
 
+  const closePreviewOnEsc = ({ keyCode }) => {
+    if (keyCode !== 27) return
+
+    closePreview({ target: previewContainer })
+  }
+
+  const closePreview = ({ target }) => {
+    if (target !== previewContainer) return
+
+    const img = previewContainer.querySelector('img')
+    previewContainer.removeChild(img)
+
+    document.body.removeEventListener('keydown', closePreviewOnEsc)
+    document.body.removeChild(previewContainer)
+  }
+
+  const openPreview = ({ target }) => {
+    if (target.nodeName !== 'IMG') return
+
+    previewContainer.appendChild(target.cloneNode())
+    previewContainer.onclick = closePreview
+
+    const label = target.getAttribute('alt')
+    labelContainer.innerText = label
+
+    document.body.addEventListener('keydown', closePreviewOnEsc)
+    document.body.appendChild(previewContainer)
+  }
+
   const load = () => {
     button = {
       next: Button('next', { onClick: go }),
@@ -57,6 +94,8 @@ const Gallery = (gallery) => {
 
     gallery.onmouseenter = () => showButton('prev') || showButton('next')
     gallery.onmouseleave = () => hideButton('prev') || hideButton('next')
+
+    gallery.onclick = openPreview
   }
 
   return { load }
